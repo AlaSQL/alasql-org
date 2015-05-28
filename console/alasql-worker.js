1bfd8bb2e1b5e7c26cadeaa36709617797f394b7
Andrey Gershun <agershun@gmail.com>
2015-05-28 22:30:04 +0300

    1

--- console/alasql-worker.js
+++ console/alasql-worker.js
@@ -1,3 +1,41 @@
+/*
+@module alasql
+@version 0.1.8
+
+AlaSQL - JavaScript SQL database
+(ñ) 2014-2015, Andrey Gershun
+
+@license
+The MIT License (MIT)
+
+Copyright (c) 2014-2015 Andrey Gershun (agershun@gmail.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+//
+// AlaSQL Workker
+// Date: 13.04.2014
+// (c) 2014-2015, Andrey Gershun
+//
+*/
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         define([], factory);
@@ -10,13 +48,20 @@
 
 /**
 	Main procedure for worker
- */
+    @function
+    @param {string} sql SQL statement
+    @param {object} params List of parameters
+    @param {callback} cb Callback function
+    @return {object} Query result
+*/
 function alasql(sql,params,cb){
+    // Increase last request id
 	var id = alasql.lastid++;
+    // Save callback
 	alasql.buffer[id] = cb;
+    // Send a message to worker
 	alasql.webworker.postMessage({id:id,sql:sql,params:params});
 };
-
 
 if (typeof importScripts === 'function') {
 	// Nothing
@@ -24,35 +69,42 @@ if (typeof importScripts === 'function') {
 
 alasql.worker = function(path, paths, cb) {
 //	var path;
-	if(typeof path == "undefined" || path === true) {
+	if(path === true) path = undefined;
+	if (typeof path == "undefined") {
 		var sc = document.getElementsByTagName('script');
 		for(var i=0;i<sc.length;i++) {
 			if (sc[i].src.substr(-16).toLowerCase() == 'alasql-worker.js') {
-				path = sc[i].src.substr(0,sc[i].src.length-16)+'alasql.min.js';
+				path = sc[i].src.substr(0,sc[i].src.length-16)+'alasql.js'; 
 				break;
-			}
-		}
-	}
+			} else if (sc[i].src.substr(-20).toLowerCase() == 'alasql-worker.min.js') {
+				path = sc[i].src.substr(0,sc[i].src.length-20)+'alasql.min.js';
+				break;
+			} else if (sc[i].src.substr(-9).toLowerCase() == 'alasql.js') {
+				path = sc[i].src; 
+				break;
+			} else if (sc[i].src.substr(-13).toLowerCase() == 'alasql.min.js') {
+				path = sc[i].src.substr(0,sc[i].src.length-13)+'alasql.min.js'; 
+				break;
+			};
+		};
+	};
+
 	if(typeof path == "undefined") {
 		throw new Error('Path to alasql.js is not specified');
 	} else if(path !== false) {
-		alasql.lastid = 0;
-		alasql.buffer = {};
 
 		var js = "importScripts('";
 			js += path;
-			js+="');\
-		self.onmessage = function(event) {\
-		alasql(event.data.sql,event.data.params, function(data){\
-		postMessage({id:event.data.id, data:data});\
-		});\
-		}";
+			js+="');self.onmessage = function(event) {"+
+		"alasql(event.data.sql,event.data.params, function(data){"+
+		"postMessage({id:event.data.id, data:data});});}";
 
 		var blob = new Blob([js], {"type": "text\/plain"});
 		alasql.webworker = new Worker(URL.createObjectURL(blob));
 
 		alasql.webworker.onmessage = function(event) {
 			var id = event.data.id;
+//			console.log('onmessage',alasql.buffer,id);
 			alasql.buffer[id](event.data.data);
 			delete alasql.buffer[id];
 		};
@@ -71,17 +123,20 @@ alasql.worker = function(path, paths, cb) {
 	} else if(path === false) {
 		delete alasql.webworker;
 		return;
-	} 
-}
+	}; 
+};
 
-}
+};
 
 
+/* WebWorker */
+/** @type {number} */
+alasql.lastid = 0;
+
+/** @type {object} */
+alasql.buffer = {};
 
 alasql.worker();
 
 return alasql;
 }));
-
-
-//# sourceMappingURL=alasql-worker.js.map
